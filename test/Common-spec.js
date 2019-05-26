18d99eef3a2cfbd98a76edb6fe98d439abfd0f28
John Ghidiu <john@rmdashrf.org>
2019-05-26 16:48:36 -0400

    Added dist/Promiseifyish.es6.js to tests

--- test/Common-spec.js
+++ test/Common-spec.js
@@ -1,7 +1,10 @@
+// Get the runner
 let runner = require('./CommonRunner.js');
 
+// Test each file (source and all dist files)
 [
     require('../src/Promiseifyish.js'),
+    require('../dist/Promiseifyish.es6.js'),
     require('../dist/Promiseifyish.js'),
     require('../dist/Promiseifyish.min.js')
 ].forEach(runner.run);
